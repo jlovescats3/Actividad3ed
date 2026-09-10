0e1e80a0c516fa23cce68107b0ff9adaf0face98
Ivonne Tellez <jackytellezarjona@gmail.com>
2026-09-09 23:26:08 -0600

    Update Fibonacci.java
    Removed comments from the Fibonacci calculation and initialization methods.

--- src/main/java/com/mycompany/actividad3ed/Fibonacci.java
+++ src/main/java/com/mycompany/actividad3ed/Fibonacci.java
@@ -6,17 +6,14 @@ public class Fibonacci {
 
     public static long calculate(int n) {
 
-        // Si ya calculamos este valor, lo regresamos
         if (memoria[n] != -1) {
             return memoria[n];
         }
 
-        // Caso base
         if (n == 0 || n == 1) {
             return n;
         }
 
-        // Caso recursivo
         memoria[n] = calculate(n - 1) + calculate(n - 2);
 
         return memoria[n];
@@ -26,7 +23,6 @@ public class Fibonacci {
 
         memoria = new long[n + 1];
 
-        // Inicializar memoria
         for (int i = 0; i <= n; i++) {
             memoria[i] = -1;
         }
